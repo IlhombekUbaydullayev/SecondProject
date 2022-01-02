71e0ee79ca58cb1a76df484da8ac057b5a4c19e6
Ilhombek <ubaydullaevilhombek681@gmail.com>
2022-01-02 21:31:02 +0500

    first

--- app/src/main/java/com/example/pushsecondactivityinjava/MainActivity.java
+++ app/src/main/java/com/example/pushsecondactivityinjava/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.pushsecondactivityinjava;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+    }
+    void initView(){
+        Button open_second = findViewById(R.id.open_second);
+        open_second.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openSecondActivity();
+            }
+        });
+    }
+    void openSecondActivity(){
+        Intent intent = new Intent(this,SecondActivity.class);
+        startActivity(intent);
     }
 }
